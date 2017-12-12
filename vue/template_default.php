4c20eaeed6b556bd51ac1affb13e4f4997baadfd
none <none>
2017-12-12 19:03:06 +0100

    pres_alpha_0.0.0.1

--- vue/template_default.php
+++ vue/template_default.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,7 +12,7 @@
 	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 	<link href="vue/style.css" rel="stylesheet" type="text/css" /> 
 
-	<!-- <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script> -->
+	<!-- <script src="https://ajax.googleapis.com/ajax/libs/angularjs/cam/js/1.6.4/angular.min.js"></script> -->
 	<!-- <link rel="stylesheet" href="jquery/jquery-ui.min.css" type="text/css" media="all" /> -->
 	<!-- <script src="jquery/external/jquery/jquery.js"></script> -->
 	<!-- <script src="jquery/jquery-ui.js"></script> -->
@@ -36,13 +37,106 @@
 					echo "<div>$value</div>";
 			}
 		}
+
+
+
 		?>
 
+    <?php
+    // require "vendor/khanamiryan/qrcode-detector-decoder/lib/QrReader.php"
+        // require __DIR__ . "/vendor/autoload.php";
+        // $qrcode = new QrReader('path/to_image');
+        // $text = $qrcode->text(); //return decoded text from 1QR Code
+        // echo $text;
+  
+
+        include_once "qr/src/test."
+
+
+// namespace Khanamiryan\QrCodeTests;
+// class QrReaderTest extends \PHPUnit_Framework_TestCase
+// {
+//     public function testText1()
+//     {
+//         $image = __DIR__ . "/qrcodes/hello_world.png";
+//         $qrcode = new \QrReader($image);
+//         $this->assertSame("Hello world!", $qrcode->text());
+//     }
+// }
+    ?>
 	</body> 
 
 
 
-</div>
+
+<!-- le commercant entre sa promotion -->
+    <br>
+    <br>
+    <br>
+
+    <?php
+
+    if (isset($_SESSION['login']) && $_SESSION['login'] != "")
+    {
+        ?>
+<h1> Commercant ajouter promotion</h1>
+
+<form action="/action_page.php">
+  Promotion: <input type="text" name="fname" placeholder="-20%"><br>
+  Article: <input type="text" name="lname" placeholder="Parfum Chanel"><br>
+  <input type="submit" value="Submit">
+</form>
+
+    <br>
+    <br>
+
+Votre promotion sera immediatement disponible aupres des utilisateur
+    <br><br><br><br><br>
+
+
+<h1> Utilisateur</h1>
+
+    Creation de QR code corespondant a la promotion et a l'identite de l'utilisateur
+    <br>
+    <br>
+    <br>
+
+	<img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Eric_Dupond_promo_parfum_20_pour_cent" />
+
+  
+
+    <br><br><br><br><br>
+
+<h1> En boutique</h1>
+
+
+    Le commercant scanne le QR code et genere un code barre (EAN-13) compatible avec son instalation existante
+    <br>
+
+    les donnes que l'utilisateur a inscrit a la connexion au wifi son remonté au serveur
+
+    <br>
+    <br>
+    <br>
+
+    <img src="http://www.barcodes4.me/barcode/c39/012345678911.png" />
+
+    <br>
+    <br>
+    <br>
+
+Le commercant applique la reduction sur le produit
+    <br><br><br><br><br>
+
+        <?php
+    }
+    else
+    {
+        echo "<h1> veuillez vous connecter pour ajouter des promotion</h1>";
+    }
+
+    ?>
+<!-- http://api.qrserver.com/v1/read-qr-code/?fileurl=http%3A%2F%2Fapi.qrserver.com%2Fv1%2Fcreate-qr-code%2F%3Fdata%3DHelloWorld -->
 
 
 </html>
